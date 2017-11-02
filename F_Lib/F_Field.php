d4733abaabb62335f5c8a2a519af7a2a0d27f681
b2vincent <bhendraut@hotmail.com>
2017-11-02 15:03:12 +0100

    Create F_Field.php

---
+++ F_Lib/F_Field.php
@@ -0,0 +1,11 @@
+<?php
+// Copyright Vincent Wartelle & Oklin.com 2016-2017
+// EntryField framework - Open sourced under MIT license  
+
+
+require_once('F_FieldConfig.php');
+require_once('F_FieldData.php');
+require_once('F_FieldField.php');
+require_once('F_FieldRender.php');
+
+// ?>
