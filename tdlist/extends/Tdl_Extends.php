43914aaa06622fb8b1d09b6d3df9ba020daa2aed
b2vincent <bhendraut@hotmail.com>
2017-11-02 15:09:35 +0100

    Create Tdl_Extends.php

---
+++ tdlist/extends/Tdl_Extends.php
@@ -0,0 +1,41 @@
+<?php
+         
+// Config
+class Ef_Config extends F_Config {}
+class Ef_Version extends F_Version {}
+class Ef_Util extends F_Util {}
+class Ef_Log extends F_Log {}
+class Ef_Session extends F_Session {}
+class Ef_Db extends F_Db {}
+class Ef_Lang extends F_Lang {}
+class Ef_Application extends F_Application {}
+// Data 
+class Ef_SqlReq extends F_SqlReq {}
+class Ef_Array extends F_Array {}
+class Ef_SqlTable extends F_SqlTable {}
+class Ef_VirtualTable extends F_VirtualTable {}
+class Ef_ReadList extends F_ReadList {}
+class Ef_List extends F_List {}
+class Ef_SessionList extends F_SessionList {}
+// Render
+class Ef_ListView extends F_ListView {}
+class Ef_Control extends F_Control {}
+class Ef_Page extends F_Page {}
+class Ef_PagePart extends F_PagePart {}
+class Ef_Route extends F_Route {}
+// Field
+// class Ef_Field extends F_Field {}
+// class Ef_FieldString extends F_FieldString {}
+// class Ef_FieldSelect extends F_FieldSelect {}
+class Ef_FieldRadio extends F_FieldRadio {}
+// class Ef_FieldDate extends F_FieldDate {}
+// class Ef_FieldText extends F_FieldText {}
+class Ef_FieldButton extends F_FieldButton {}
+// class Ef_FieldRowButton extends F_FieldRowButton {}
+// class Ef_FieldInt extends F_FieldInt {}
+class Ef_FieldAmount extends F_FieldAmount {}
+// Extended
+class Ef_TableUtil extends F_TableUtil {}
+class Ef_FieldRowIconButton extends F_FieldRowIconButton {}
+
+?>
