17081bf872c7fba0a3193845b91dc0dedb8cc73f
b2vincent <bhendraut@hotmail.com>
2017-11-02 15:17:32 +0100

    Create Tdl_Routes.php

---
+++ pages/Tdl_Routes.php
@@ -0,0 +1,14 @@
+<?php
+
+// Declare the routes of the application
+
+$newroute = new Ef_Route('TdlSimpleFieldsPage.php','tdl-simple-fields');
+$newroute = new Ef_Route('TdlItemListPage.php','tdl-item-list');
+$newroute = new Ef_Route('TdlItemUpdatePage.php','tdl-item-update');
+$newroute = new Ef_Route('TdlAdminPage.php','tdl-admin');
+$newroute = new Ef_Route('TdlLoginPage.php','tdl-login');
+
+
+
+
+?>
