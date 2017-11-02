b276d6f656894e6b7c1be55591403002ae2b29c7
b2vincent <bhendraut@hotmail.com>
2017-11-02 15:13:53 +0100

    Create Tdl_Lang_en.php

---
+++ tdlist/lang/Tdl_Lang_en.php
@@ -0,0 +1,78 @@
+<?php
+
+
+Ef_Lang::set("You are connected as %1","You are connected as %1");
+
+Ef_Lang::set('Ticket management system','Ticket management system');
+
+Ef_Lang::set('tdlit.itid','Id');
+Ef_Lang::set('tdlit.it_projid','Project');
+Ef_Lang::set('tdlit.it_statusid','Status');
+Ef_Lang::set('tdlit.it_title','Title');
+Ef_Lang::set('tdlit.it_text','Description');
+Ef_Lang::set('tdlit.it_emergency','Emergency rank');	
+Ef_Lang::set('tdlit.it_datechanged','Date changed');
+Ef_Lang::set('tdlit.it_updatedbyid','Updated by');
+Ef_Lang::set('tdlit.it_assignedtoid','Assigned to');
+Ef_Lang::set('virtual.btn_upditem','Update item');
+Ef_Lang::set('virtual.btn_delitem','Delete item');
+
+Ef_Lang::set('tdlus.usid','Id');	
+Ef_Lang::set('tdlus.us_login','Login');	
+Ef_Lang::set('tdlus.us_password','Password');	
+Ef_Lang::set('virtual.btn_add_user','Add user');	
+Ef_Lang::set('virtual.btn_del_user','Del user');
+
+
+Ef_Lang::set('tdlpr.prid','Id');	
+Ef_Lang::set('tdlpr.pr_title','Title');	
+Ef_Lang::set('virtual.btn_add_proj','Add project');	
+Ef_Lang::set('virtual.btn_del_proj','Del project');	
+
+Ef_Lang::set('tdlst.stid','Id');	
+Ef_Lang::set('tdlst.st_title','Title');
+Ef_Lang::set('tdlst.st_icon','Icon');	
+Ef_Lang::set('tdlst.st_isactive','Is active');
+Ef_Lang::set('virtual.btn_add_stat','Add status');	
+Ef_Lang::set('virtual.btn_del_stat','Del status');	
+
+Ef_Lang::set('tdlap.apid','Id');
+Ef_Lang::set('tdlap.ap_title','Title');	
+Ef_Lang::set('tdlap.ap_subtitle','Subtitle');
+Ef_Lang::set('tdlap.ap_admintype','Admin presentation type');
+
+Ef_Lang::set('vlog.login','Login');	
+Ef_Lang::set('vlog.password','Password');	
+Ef_Lang::set('virtual.btn_connect_user','Connect'); 
+Ef_Lang::set('virtual.btn_disconnect_user','Disconnect');	
+
+Ef_Lang::set('virtual.btn_add_item', 'Add item');
+Ef_Lang::set('virtual.btn_del_item', 'Del item');
+
+Ef_Lang::set('vsimp.name','Name'); 
+Ef_Lang::set('vsimp.street','Street'); 
+Ef_Lang::set('vsimp.visible','Visible'); 
+Ef_Lang::set('vsimp.checked','Checked');  
+Ef_Lang::set('vsimp.amount','Amount');
+ 
+ 
+Ef_Lang::set("Think, process, solve, Enjoy !", 
+    "Think, process, solve, Enjoy !
+    <i class=\"fa fa-lg fa-cloud text-muted\"></i>
+    <i class=\"fa fa-lg fa-gears text-muted\"></i>
+    <i class=\"fa fa-lg fa-check text-muted\"></i>
+    <i class=\"fa fa-lg fa-smile-o text-muted\"></i>");
+
+
+Ef_Lang::set('Logintext','
+    <h4>Welcome to Tdlist ! </h4>
+    <br><br>
+    Demo application of the Entryfield framework for PHP. 
+'); 
+
+Ef_Lang::set('Cheattext','
+    <br>
+    <b>Initial users are entryfield, roger and anna, with password <i>abcdef</i></b>
+'); 
+
+?>
