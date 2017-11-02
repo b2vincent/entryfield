cff102d4d9f4fd8d180b760b348348830776d91d
b2vincent <bhendraut@hotmail.com>
2017-11-02 15:11:46 +0100

    Create ErrorHandler.php

---
+++ tdlist/helpers/ErrorHandler.php
@@ -0,0 +1,65 @@
+<?php
+
+// Thanks to http://stackoverflow.com/questions/1475297/phps-white-screen-of-death
+// if we include this file, we get a chance to see the errors
+// ----------------------------------------------------------------------------------------------------
+// - Display Errors
+// ----------------------------------------------------------------------------------------------------
+ini_set('display_errors', 'On');
+ini_set('html_errors', 0);
+
+// ----------------------------------------------------------------------------------------------------
+// - Error Reporting
+// ----------------------------------------------------------------------------------------------------
+error_reporting(-1);
+
+// ----------------------------------------------------------------------------------------------------
+// - Shutdown Handler
+// ----------------------------------------------------------------------------------------------------
+function shutDownHandler()
+{
+    if(@is_array($error = @error_get_last()))
+    {
+        return(@call_user_func_array('errorHandler', $error));
+    };
+
+    return(TRUE);
+};
+
+register_shutdown_function('shutDownHandler');
+
+// ----------------------------------------------------------------------------------------------------
+// - Error Handler
+// ----------------------------------------------------------------------------------------------------
+function errorHandler($type, $message, $file, $line)
+{
+    $_ERRORS = Array(
+        0x0001 => 'E_ERROR',
+        0x0002 => 'E_WARNING',
+        0x0004 => 'E_PARSE',
+        0x0008 => 'E_NOTICE',
+        0x0010 => 'E_CORE_ERROR',
+        0x0020 => 'E_CORE_WARNING',
+        0x0040 => 'E_COMPILE_ERROR',
+        0x0080 => 'E_COMPILE_WARNING',
+        0x0100 => 'E_USER_ERROR',
+        0x0200 => 'E_USER_WARNING',
+        0x0400 => 'E_USER_NOTICE',
+        0x0800 => 'E_STRICT',
+        0x1000 => 'E_RECOVERABLE_ERROR',
+        0x2000 => 'E_DEPRECATED',
+        0x4000 => 'E_USER_DEPRECATED'
+    );
+
+    if(!@is_string($name = @array_search($type, @array_flip($_ERRORS))))
+    {
+        $name = 'E_UNKNOWN';
+    };
+
+    return(print(@sprintf("<br> %s Error in file %s at line %d: %s\n", $name, @basename($file), $line, $message)));
+};
+
+$old_error_handler = set_error_handler("errorHandler");
+
+// other php code
+
