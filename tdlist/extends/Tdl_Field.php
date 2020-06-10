2eca0e52e41220fb2cef7c611166d1329e18ac50
b2vincent <bhendraut@hotmail.com>
2020-06-10 09:03:24 +0200

    Upgrade to 2.12
    Creating file Ef_Field_Extends

--- tdlist/extends/Tdl_Field.php
+++ tdlist/extends/Tdl_Field.php
@@ -58,41 +58,6 @@ class Ef_FieldChecked extends Ef_FieldInt
 }
 
 
-// Specialized to add bootstrap behaviour
-class Ef_Field extends F_Field 
-{
-
-	// Add label to a field value of a field  - bootstrap mode
-    protected function addLabel($fieldname, $stringvalue)
-    {
-        // $fieldlabel = F_Lang::get($fieldname);
-        $fieldlabel = Ef_Lang::get($this->getName());
-        
-        return "$fieldlabel $stringvalue";
-        $labeledstring = ("  
-            <div class=\"form-group\">
-            <label for=\"$fieldname\" class=\"col-sm-3 control-label\">$fieldlabel</label>
-            $stringvalue        
-            </div>            
-        ");
-        return $labeledstring;
-    }
-
-    // Add placeholder and class, bootstrap way
-    public function memToEditHtml ($value, $parms=array())
-    {
-        $editField = parent::memToEditHtml($value, $parms);
-        $fieldlabel = Ef_Lang::get($this->getName()); 
-
-        $placeholdertext = "placeholder=\"$fieldlabel\"";
-        $classtext = 'class="form-control"';         
-        
-        return (str_replace('data-dummy="dummy"', $placeholdertext.' '.$classtext, $editField));
-    
-    }
-    
-  
-}
 
 
 // Specialized to add bootstrap behaviour
